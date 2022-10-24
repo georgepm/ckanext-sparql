950904de02d341538c0077ef0d0b1f7a4cddda9b
Jakub Dorňák <jakub.dornak@misli.cz>
2022-10-25 00:28:32 +0200

    Update to ckan 2.9

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import find_packages, setup
 
-version = '0.1'
+version = "0.1"
 
 setup(
-	name='ckanext-sparql',
-	version=version,
-	description="Sparql_Point",
-	long_description="""\
-	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='Jorge Pantoja',
-	author_email='jorgepantojam@gmail.com',
-	url='http://data.upf.edu',
-	license='AGPL',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.sparql'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points="""
+    name="ckanext-sparql",
+    version=version,
+    description="Sparql_Point",
+    long_description="",
+    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords="",
+    author="Jorge Pantoja",
+    author_email="jorgepantojam@gmail.com",
+    url="http://data.upf.edu",
+    license="AGPL",
+    packages=find_packages(),
+    namespace_packages=["ckanext", "ckanext.sparql"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "ckan",
+        "simplejson",
+        "SPARQLWrapper",
+    ],
+    entry_points="""
         [ckan.plugins]
-	# Add plugins here, eg
-	# myplugin=ckanext.sparql:PluginClass
-	sparql_interface = ckanext.sparql.plugin:SparqlPlugin
-	""",
+    # Add plugins here, eg
+    # myplugin=ckanext.sparql:PluginClass
+    sparql_interface = ckanext.sparql.plugin:SparqlPlugin
+    """,
 )
